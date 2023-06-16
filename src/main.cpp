8d1791e4461b3b1852a078ed3b9e3c32b0c042e8
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-06-16 13:36:58 +0200

    init test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "Config.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(BAUDRATE);
+
+  while (!Serial)
+    ;
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  Serial.println("ok");
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
