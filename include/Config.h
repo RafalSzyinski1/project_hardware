7488ab2669b695b5e408ed38918ae53dd5dc9cfd
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-06-17 18:47:31 +0200

    create key adder

--- include/Config.h
+++ include/Config.h
@@ -5,9 +5,11 @@
 #define BAUDRATE 115200
 
 // Pins
-#define YELLOW_LED 4
-#define GREEN_LED 0
-#define RED_LED 2
-#define BUTTON_PIN 5
+#define YELLOW_LED D2
+#define GREEN_LED D3
+#define RED_LED D4
+#define BUTTON_PIN D1
+#define SS_PIN D8
+#define RST_PIN D0
 
 #endif // _CONFIG_H_
